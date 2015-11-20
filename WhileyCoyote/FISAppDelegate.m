0a870f57fbf13ebab60d33cd6a907fd573fdaf2d
hkxnyc <hannah.kim@flatironschool.com>
2015-11-19 20:12:40 -0500

    whileyCoyote

--- WhileyCoyote/FISAppDelegate.m
+++ WhileyCoyote/FISAppDelegate.m
@@ -10,6 +10,18 @@
      * Write your loop here!
     
      */
+    NSUInteger steps = 0;
+    NSInteger anvil = arc4random_uniform(25) + 26; //generate random numbers from 26 to 50
+    do {
+        if(steps%10 == 0 && steps>0) {
+            NSLog(@"YOU'RE CUCKOO!");
+        }
+        if (steps==50){
+            NSLog(@"SMASH!");
+        }
+        NSLog(@"%lu. Meep! Meep!",steps);
+        steps++;
+    }while (steps<=anvil);
     
     // do not alter
     return YES;  //
